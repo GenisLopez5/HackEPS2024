625194a0e9cd82e0453469a1354c0732bb62aa90
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2024-11-24 06:33:24 +0100

    added colorful plot

--- web/resources/views/parking.blade.php
+++ web/resources/views/parking.blade.php
@@ -137,17 +137,22 @@
                                 data: {
                                     labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
                                     datasets: [{
-                                        label: 'Predicted Occupancy',
-                                        data: [{{ implode(',', $parking->predictions) }}],
+                                        data: {!! json_encode($parking->predictions) !!},
                                         backgroundColor: '#3b82f6'
                                     }]
                                 },
                                 options: {
+                                    
                                     responsive: true,
                                     maintainAspectRatio: false,
                                     plugins: {
                                         legend: {
                                             display: false
+                                        },
+                                        title: {
+                                            display: true,
+                                            text: 'Real & Prediction Data',
+                                            color: '#D1D5DB'
                                         }
                                     },
                                     scales: {
